fec55d988483d38df082f9941d0838ec274c035f
emredalkiran <emre59dalkiran@gmail.com>
2020-12-17 11:05:02 +0300

    Express app instantiated with service container

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,17 +9,14 @@ async function init() {
   env.config()
   const connectionURL = `mongodb://${process.env.URL}`
   const port = process.env.PORT || 3000
-  const databaseConnection = new MongoConnection()
-  
+  const databaseConnection = new MongoConnection() 
   const app = new App()
-  
-  app.configureApp()
-
   try {
     const db = await databaseConnection.connect(connectionURL, process.env.DATABASE_NAME!)
     console.log(`Connected to ${process.env.DATABASE_NAME} database `)
     console.log('Starting server...')
     const serviceContainer = new ServiceContainer(db)
+    app.configureApp(serviceContainer)
     const server = http.createServer(app.instance)
     server.listen({host: 'localhost', port: port},()=> console.log(`Server listening on port ${port} at localhost`))
   } catch (err) {
