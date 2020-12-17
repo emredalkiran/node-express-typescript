296063fcd2c1389e2186893adf29b7e2bc35dc10
emredalkiran <emre59dalkiran@gmail.com>
2020-12-17 11:05:29 +0300

    Helpers added to service container

--- backend/src/service-container.ts
+++ backend/src/service-container.ts
@@ -1,27 +1,27 @@
-import { hash } from 'bcrypt'
 import { Db } from 'mongodb'
 import UserRepository from './user/user-repository'
 import UserService from './user/user-service'
-import { Helper } from './utils/interfaces'
 import { ErrorMesages, errorMessages } from './utils/error-messages'
-import AuthService from './auth/auth'
+import Authenticator from './auth/auth'
+import HashHelper, { Hash } from './utils/hash'
+
+interface Helper {
+  hash: Hash,
+  auth: Authenticator
+}
 
 export default class ServiceContainer {
-  db: Db
   userRepository: UserRepository
   userService: UserService
   helpers: Helper
   errorMessages: ErrorMesages
   constructor(db: Db) {
-    this.db = db
     this.userRepository = new UserRepository(db)
     this.helpers = { 
-      hash: new Hash(),
-      auth: new AuthService(this.userRepository)
+      hash: new HashHelper(),
+      auth: new Authenticator(this.userRepository)
     }
     this.userService = new UserService(this.userRepository, this.helpers.hash)
-
-    this.authService = new AuthService(this.helpers.hash)
     this.errorMessages = errorMessages
   }
 }
