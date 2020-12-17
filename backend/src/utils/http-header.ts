646ffa06e6a981a59dc24b32300a6fb36bfe27ab
emredalkiran <emre59dalkiran@gmail.com>
2020-12-17 11:13:09 +0300

    Http header and status contstants added

---
+++ backend/src/utils/http-header.ts
@@ -0,0 +1,11 @@
+export const httpHeader = {
+  json: 
+      {'Content-Type':'application/json'}
+}
+
+export const statusCode = {
+  success: 200,
+  badRequest: 400,
+  unauthorized: 401,
+  internalError: 500
+}
